4a9a47eadc7b6f0afdf59f39111344108bb186a8
bugalter168 <bugalter19882204@gmail.com>
2025-07-17 11:54:45 +0300

    portfolio
    add html
    add style
    add js

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const serviceModal = document.querySelectorAll(".service-modal");
+const learnMoreBtm = document.querySelectorAll(".learn-more-btn");
+const modalCloceBtn = document.querySelectorAll(".modal-close-btn");
+
+const modal = function(modalClick){
+    serviceModal[modalClick].classList.add("active")
+}
+
+learnMoreBtm.forEach((button, i) => {
+    button.addEventListener("click", function(){
+        modal(i)
+    })   
+})
+
+modalCloceBtn.forEach(button => {
+    button.addEventListener("click", () =>{
+        serviceModal.forEach(modal => {
+            modal.classList.remove("active")
+        })
+    })
+})
+
+// Portfolio section modal
+
+const portfolioModals = document.querySelectorAll(".portfolio-modal");
+const imgCard = document.querySelectorAll(".img-card");
+const portfolioCloceBtn = document.querySelectorAll(".portfolio-close-btn");
+
+const portfolio = function (modalClick) {
+  portfolioModals[modalClick].classList.add("active");
+};
+
+imgCard.forEach((button, i) =>{
+    button.addEventListener("click", () => {
+        portfolio(i)
+    })
+})
+
+portfolioCloceBtn.forEach(button => {
+    button.addEventListener("click", () => {
+        portfolioModals.forEach(modalView =>{
+            modalView.classList.remove("active");
+        }) 
+        
+    })
+})
