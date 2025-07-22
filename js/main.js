1efb06104dbfcebdaf86a6f973ddaa141a6eb8f7
bugalter168 <bugalter19882204@gmail.com>
2025-07-22 15:57:29 +0300

    Contact section
    add tags
    add style

--- js/main.js
+++ js/main.js
@@ -44,3 +44,18 @@ portfolioCloceBtn.forEach(button => {
         
     })
 })
+
+// добавляем слайди
+var swiper = new Swiper(".client-swipper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
